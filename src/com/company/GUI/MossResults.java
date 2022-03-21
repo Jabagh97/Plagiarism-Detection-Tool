6973b2509bd934a390d66a7686c775491fabca7a
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2022-03-21 19:39:36 +0300

    test

--- src/com/company/GUI/MossResults.java
+++ src/com/company/GUI/MossResults.java
@@ -1,4 +1,67 @@
 package com.company.GUI;
 
-public class MossResults {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+public class MossResults extends JFrame {
+
+    public MossResults() {
+        initUI();
+    }
+
+    private void initUI() {
+
+        CategoryDataset dataset = createDataset();
+
+        JFreeChart chart = createChart(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
+
+        pack();
+        setTitle("Moss chart");
+        setLocationRelativeTo(null);
+
+    }
+
+    private CategoryDataset createDataset() {
+
+        var dataset = new DefaultCategoryDataset();
+        dataset.setValue(50, "", "80");
+
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(CategoryDataset dataset) {
+
+        JFreeChart barChart = ChartFactory.createBarChart(
+                "Moss Results",
+                "Similarity",
+                "Number of Pairs",
+                dataset,
+                PlotOrientation.VERTICAL,
+                false, true, false);
+
+        return barChart;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new MossResults();
+            ex.setVisible(true);
+        });
+    }
 }
