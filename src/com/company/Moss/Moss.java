23e3756e9d8383f36c37604007c4e21ddc72eb45
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2022-03-24 23:39:30 +0300

    Directory is working now

--- src/com/company/Moss/Moss.java
+++ src/com/company/Moss/Moss.java
@@ -1,6 +1,7 @@
 package com.company.Moss;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -33,37 +34,15 @@ public class Moss  {
     public String fileNames="";
     String file = "";
     String path = "";
-    String filesPathCommand = "cd ";
+    String filesPathCommand = "";
     String mossCommand = "perl moss.pl -l cc ";
+
     //Parsing Command Output
-    /* void exec() throws IOException {
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec("cd \"C:\\Users\\Jabagh angok\\OneDrive\\Documents\\aDummy\" && dir");
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(proc.getInputStream()));
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(proc.getErrorStream()));
-
-// Read the output from the command
-        System.out.println("Here is the standard output of the command:\n");
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
-
-// Read any errors from the attempted command
-        System.out.println("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }
-
-}
-*/
     public void execute() throws Exception {
-        System.out.println(filesPathCommand);
-        System.out.println(mossCommand);
-        ProcessBuilder builder = new ProcessBuilder( "cmd.exe", "/c",mossCommand);
-
+        //System.out.println(filesPathCommand);
+        //System.out.println(mossCommand);
+        ProcessBuilder builder = new ProcessBuilder( "cmd.exe", "/c", mossCommand);
+        builder.directory(new File(filesPathCommand));
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -79,7 +58,6 @@ public class Moss  {
     {
         int index = cmdOutput.indexOf(":")-4; //this finds the first occurrence of "."
         String url = cmdOutput.substring(index);
-
     }
     //Get Similarity Percentage
      void getPercentage(String input)
@@ -88,7 +66,6 @@ public class Moss  {
         String simPercent = input.substring(index);
         simPercent = simPercent.substring(2,simPercent.length()-4);
         int output = Integer.parseInt(simPercent);
-
     }
     //Get File Names
      String getFileName(String input)
@@ -105,27 +82,20 @@ public class Moss  {
         this.path=path;
     }
 
-
      public void runMoss() throws Exception {
          //cd to files path
          ArrayList<String> names = new ArrayList<>();
          int index = path.lastIndexOf('\\');
          path = path.substring(0,index);
-
          //Getting the files path
          filesPathCommand += path;
          //Completing Moss command with file names
          mossCommand += fileNames;
-
          //Storing file names in a list
          for(String s :fileNames.split(" ")){
              names.add(s);
              System.out.println(s);
          }
-
-
          execute();
-
-
      }
 }
